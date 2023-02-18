ee79c14cf5edfd25112278aacca006aa673d6271
Sameh Edward <edw21629@spengergasse.at>
2023-02-18 11:43:38 +0100

    Initial commit

---
+++ src/test/java/at/pro/photographer/PhotographerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.pro.photographer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotographerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
