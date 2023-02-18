3c5e43b3a50517405f790ce90c89d3aad76d3ed1
Sameh Edward <edw21629@spengergasse.at>
2023-02-18 12:02:16 +0100

    Photo Class erstellt

---
+++ src/main/java/at/pro/photographer/domain/Orientation.java
@@ -0,0 +1,4 @@
+package at.pro.photographer.domain;
+
+public enum Orientation {
+}
--- src/main/java/at/pro/photographer/domain/Photo.java
+++ src/main/java/at/pro/photographer/domain/Photo.java
@@ -1,5 +1,14 @@
 package at.pro.photographer.domain;
 
+import java.time.LocalDateTime;
+
 public class Photo
 {
+    private String fileName;
+    private String name;
+    private LocalDateTime creationTS;
+    private Integer width;
+    private Integer height;
+    private Orientation orientation;
+
 }
