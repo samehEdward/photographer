6d1635d2834a839af3c12c9fd29d65cc2637403f
Sameh Edward <edw21629@spengergasse.at>
2023-02-20 19:22:25 +0100

    photo Repo wurde erstelltÂ

--- src/main/java/at/pro/photographer/persistence/PhotoRepo.java
+++ src/main/java/at/pro/photographer/persistence/PhotoRepo.java
@@ -1,4 +1,9 @@
 package at.pro.photographer.persistence;
 
-public interface PhotoRepo {
+import at.pro.photographer.domain.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PhotoRepo extends JpaRepository<Photo, Long> {
 }
