d7f5acc19cd9cd99999b886fbb4312062038e5f1
Sameh Edward <edw21629@spengergasse.at>
2023-02-20 19:37:25 +0100

    Photo Repository testen

---
+++ src/test/java/at/pro/photographer/persistence/PhotoRepoTest.java
@@ -0,0 +1,22 @@
+package at.pro.photographer.persistence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assumptions.assumeThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class PhotoRepoTest {
+
+    @Autowired
+    private PhotoRepo photoRepo;
+
+    @BeforeEach
+    void setup()
+    {
+        assumeThat(photoRepo).isNotNull();
+    }
+
+}
