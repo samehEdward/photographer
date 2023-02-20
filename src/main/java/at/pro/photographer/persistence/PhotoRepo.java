8ed66bc0ff2bb3c88f76a51a11c00bae7d9e4b2e
Sameh Edward <edw21629@spengergasse.at>
2023-02-20 21:34:58 +0100

    app properties angepasst

--- src/main/java/at/pro/photographer/persistence/PhotoRepo.java
+++ src/main/java/at/pro/photographer/persistence/PhotoRepo.java
@@ -4,6 +4,11 @@ import at.pro.photographer.domain.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PhotoRepo extends JpaRepository<Photo, Long> {
+
+    @Override
+    List<Photo> findAll();
 }
